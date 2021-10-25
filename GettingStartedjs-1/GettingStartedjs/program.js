fcdfc008096d3b21c9e429546af53e08cb2d3d1a
Kim Steen Bekke <65952389+KimSteenBekke@users.noreply.github.com>
2021-10-25 10:40:58 +0200

    Add files via upload

---
+++ GettingStartedjs-1/GettingStartedjs/program.js
@@ -0,0 +1,8 @@
+var lunch = "pizza";
+console.log(lunch);
+console.log(lunch);
+console.log(4);
+
+var name = "Scott";
+name = "Allen";
+console.log(name);
---
+++ GettingStartedjs-1/GettingStartedjs/program2.js
@@ -0,0 +1,8 @@
+var totalCost = 14.99;
+
+//Enter a number to represent the sizeOfParty//
+var sizeOfParty = process.argv[2];
+
+var averageCost = totalCost / sizeOfParty;
+
+console.log("$ " + averageCost);
