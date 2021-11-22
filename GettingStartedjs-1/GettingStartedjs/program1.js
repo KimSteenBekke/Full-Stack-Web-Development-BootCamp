55087747ad23e8c3b11b658ffecb9145bdcdbc80
Kim Steen Bekke <65952389+KimSteenBekke@users.noreply.github.com>
2021-11-22 18:25:34 +0100

    Delete GettingStartedjs-1/GettingStartedjs directory

--- GettingStartedjs-1/GettingStartedjs/program1.js
+++
@@ -1,8 +0,0 @@
-var lunch = "fish and chips";
-console.log(lunch);
-console.log(lunch);
-console.log(4);
-
-var name = "Scott";
-name = "Allen";
-console.log(name);
--- GettingStartedjs-1/GettingStartedjs/program3.js
+++
@@ -1,8 +0,0 @@
-var totalCost = 14.99;
-
-//Enter a number to represent the sizeOfParty//
-var sizeOfParty = process.argv[2];
-
-var averageCost = totalCost / sizeOfParty;
-
-console.log("EUR " + averageCost);
